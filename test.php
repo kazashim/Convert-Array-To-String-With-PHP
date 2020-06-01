2dfd10ddfcbd616d2e007e4ce1415c991de423e7
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-06-01 21:19:39 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+$cynPost = [
+     "user" => "CodeWall",
+     "post" => "Check out our latest post on CodeWall",
+     "timestamp" => date('Y-m-d H:i:s'),
+     "share_count" => 6,
+     "like_count" => 9
+];
+
+echo implode(" ", $cynPost);
